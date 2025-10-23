2ba49126d69c8ea1e29324944db2a33952edfd7a
Meenatchi Sundari Muthirulappan <85951411+Meenatchisundari@users.noreply.github.com>
2025-10-23 15:45:48 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="AI_STUDDY_BUDDY",
+    description="An AI-powered study assistant to help students with their studies.",
+    version="0.1",
+    author="Meenatchi Sundari",
+    packages=find_packages(),
+    install_requires = requirements,
+)
